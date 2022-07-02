2dc10dd360614476e604cb0c527a7f559d1ff254
phillipashe <ashephillip@gmail.com>
2022-07-02 14:15:15 -0400

    setup sqlite

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const routes = require('./routes/game');
+const setupDB = require('./services/db_setup');
+
+setupDB();
 
 const app = express();
 app.use('/game', routes);
